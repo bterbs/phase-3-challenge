7f7b09d9edafcdc3c071d87ffe1298e2c8c3e8f6
B Terbush <brittanyterbush@gmail.com>
2017-11-20 22:41:25 -0800

    update db and load-data to match spec requirements.

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -5,10 +5,31 @@ VALUES
 INSERT INTO shoppers (shopperID, name)
 VALUES
 (2, 'Miles');
+INSERT INTO shoppers (shopperID, name)
+VALUES
+(3, 'Nathan');
+INSERT INTO shoppers (shopperID, name)
+VALUES
+(4, 'Krystal');
+INSERT INTO shoppers (shopperID, name)
+VALUES
+(5, 'Kitty');
 
 
 INSERT INTO orders (orderID, shopperID, total_cost)
 VALUES
- (1, 1, 49.99);
+ (1, 1, 9.99);
+INSERT INTO orders (orderID, shopperID, total_cost)
+VALUES
+(2, 3, 11.50);
+INSERT INTO orders (orderID, shopperID, total_cost)
+VALUES
+(3, 3, 13.99);
+INSERT INTO orders (orderID, shopperID, total_cost)
+VALUES
+(4, 4, 49.99);
+INSERT INTO orders (orderID, shopperID, total_cost)
+VALUES
+(5, 5, 4.99);
 
 \copy grocery (name, price, section) FROM 'grocery.csv' WITH DELIMITER ',' CSV HEADER
